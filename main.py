0571555c59f137216408d7cbf2483511acdc5d4f
frankqcz <frankqczhao@gmail.com>
2017-11-12 15:53:54 -0800

    earn all the desktop search awards
    add while loop to earn all the points

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ Firefox and Edge browsers work.
 Chrome will crash.
 
 Update log:
+
     @11/10/2017
     successfully login to outlook accout.
 
     @11/11/2017
     add current reward points checker
     add desktop_search_awards function
+
+    @11/12/2017
+    add while loop to keep earning desktop search awards until 150 points
 """
 
 
@@ -46,57 +50,82 @@ class account(object):
         self.browser.get("https://bing.com")
         time.sleep(10)
         assert "Bing" in self.browser.title
+
     def earn_desktop_search_awards(self):
         ### print current points ###
+        rewards = self.browser.find_element_by_id("id_rc")
+        self.current_points = int(rewards.text)
+        print("The current points are: ", rewards.text)
+        initial_points = self.current_points
+        target_points = 150
+        used_items = []
+        while ((self.earned_points < target_points) and (self.page_searches<=40)):
+
+            ### find the search bar ###
+            searchbar = self.browser.find_element_by_id("sb_form_q")
+
+            ### delete everything in the search bar ###
+            searchbar.send_keys(Keys.CONTROL, 'a')
+            searchbar.send_keys(Keys.DELETE)
+            time.sleep(3)
+
+            ### generate a random term to searchbar ###
+            item = search_items[random.randrange(0, len(search_items))]
+            if item not in used_items:
+                used_items.append(item)
+                print("Searching for:\t", item)
+                try:
+                    ### enter the search item ###
+                    searchbar.send_keys(item)
+                    time.sleep(2)
+                    searchbar.send_keys(Keys.ENTER)
+                    self.page_searches += 1
+                    time.sleep(random.randint(5,20))
+                    try:
+                        ### find a link and click into it ###
+                        link = self.browser.find_element_by_partial_link_text(item[1:3])
+                        print("Click the link:\t", link.text)
+                        link.click()
+                        time.sleep(random.randint(5,10))
+                        self.browser.get("https://bing.com")
+                        time.sleep(5)
+                    except:
+                        print("No Links")
+                except:
+                    print("Problem sending search item")
+
+            time.sleep(5)
+            ### Calculate the earned points ###
+            rewards = self.browser.find_element_by_id("id_rc")
+            self.current_points = int(rewards.text)
+            print("The current points are: ", rewards.text)
+            self.earned_points = self.current_points - initial_points
+            print("Earned points are: ", self.earned_points)
+            print("Searched times: ", self.page_searches)
 
 
-        ### find the search bar ###
-        searchbar = self.browser.find_element_by_id("sb_form_q")
-
-        ### delete everything in the search bar ###
-        searchbar.send_keys(Keys.CONTROL, 'a')
-        searchbar.send_keys(Keys.DELETE)
-        time.sleep(3)
-
-        ### generate a random term to searchbar ###
-        item = search_items[random.randrange(0, len(search_items))]
-        print("Searching for:\t", item)
-
-        try:
-            ### enter the search item ###
-            searchbar.send_keys(item)
-            time.sleep(2)
-            searchbar.send_keys(Keys.ENTER)
-            self.page_searches += 1
-            time.sleep(random.randint(5,20))
-
-            try:
-                ### find a link and click into it ###
-                link = self.browser.find_element_by_partial_link_text(item[1:3])
-                print("Click the link:\t", link.text)
-                link.click()
-                time.sleep(random.randint(3,8))
-                self.browser.back()
-            except:
-                print("No Links")
-        except:
-            print("Problem sending search item")
-        time.sleep(3)
 
     def get_current_points(self):
         self.browser.get("https://bing.com")
         time.sleep(5)
         rewards = self.browser.find_element_by_id("id_rc")
         self.current_points = int(rewards.text)
-        return self.current_pints()
+        return self.current_points
 
     def quit(self):
         self.browser.quit()
 
 
 if __name__ == "__main__":
-    search_items = ["facebook", "twitter", "Linkedin", "Glassdoor","Indeed",\
-                    "microsoft", "Google", "map","yelp", "google scholar"]
+    search_items = ["what is facebook", "how twitter", "Linkedin", "Glassdoor","Indeed",\
+                    "microsoft", "Google", "map","yelp", "google scholar",\
+                    "UCI location", "UCLA email", "USC fun", "Team up", \
+                    "Starcraft I", "trump", "white left", "Friends"\
+                    "frankqcz", "Github usage", "APDSL website", "apple music",\
+                    "Reddit login", "Dictionary text", "shengciben",\
+                    "Zootopia on screen", "Tustin AMC", "Maxlinear corp", \
+                    "Broadcom buy qualcomm", "Xinks stock", "Des Auto", \
+                    "when game of throne ends", "Bing vs Google"]
     email = "email"
     password = "password"
     milkBot = account(email,password)
